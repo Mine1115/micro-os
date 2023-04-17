6d75461a3e9ec65757a2bee186d0b21f3ca98313
Mine1115 <126138054+Mine1115@users.noreply.github.com>
2023-04-17 17:03:56 -0500

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -174,9 +174,13 @@ basic.show_leds("""
         # # # # #
 """)
 basic.clear_screen()
-basic.show_string("Hello worlld")
-in_game = "no"
+mineu_sleted = 1
+# 0 = no
+# 1 = yes
 
 def on_forever():
-    pass
+    in_game = 0
+    while in_game == 0:
+        while mineu_sleted == 1:
+            pass
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -174,8 +174,32 @@ basic.showLeds(`
     # # # # #
     `)
 basic.clearScreen()
-basic.showString("Hello worlld")
-let in_game = "no"
+let mineu_sleted = 1
+// 0 = no
+// 1 = yes
 basic.forever(function () {
-	
+    let in_game = 0
+    while (in_game == 0) {
+        while (mineu_sleted == 1) {
+            basic.showLeds(`
+                # . # . #
+                . # # # .
+                # # . # #
+                . # # # .
+                # . # . #
+                `)
+            if (input.buttonIsPressed(Button.B)) {
+                mineu_sleted = 2
+            }
+        }
+        while (mineu_sleted == 2) {
+            basic.showLeds(`
+                # . # . #
+                . # # # .
+                # # . # #
+                . # # # .
+                # . # . #
+                `)
+        }
+    }
 })
